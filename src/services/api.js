f028180cb6a1027fa5a82b3392b977e10b1a99fc
Gabriel Ramirez <gabrielrs874@gmail.com>
2025-07-17 18:59:24 -0400

    Frontend para sistema de asistencia

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: import.meta.env.PERRITO_DE_ARROZ,
+  baseURL: "https://sistema-proveedores-backend-production.up.railway.app/api",
 });
 
 instance.interceptors.request.use((config) => {
